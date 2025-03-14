368288c0f884d4bea06396d822d23d35daf707c6
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-03-14 12:25:30 +0330

    refactor: minor updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Button } from "@/components/ui/button"
-
+import { MailOpen } from 'lucide-react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,7 +10,9 @@ function App() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+      <Button>
+        <MailOpen /> Login with Email
+      </Button>
     </div>
   )
 }
