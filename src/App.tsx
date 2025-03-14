4e6478ba99ac7e124d73da420ade801c57dd4954
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-03-14 12:17:25 +0330

    feat: install shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,9 +9,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
